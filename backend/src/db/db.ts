e237bf457884e9c04574b00ebacc60f2175442fa
Tunde James <59791126+tunde-james@users.noreply.github.com>
2026-01-23 01:48:58 +0100

    Create docker file for db and create a connection pull using pg

---
+++ backend/src/db/db.ts
@@ -0,0 +1,31 @@
+import { Pool, QueryResult, QueryResultRow } from 'pg';
+
+import { env } from '../config/env.config';
+import { logger } from '../lib/logger';
+
+export const pool = new Pool({
+  host: env.DB_HOST,
+  port: Number(env.DB_PORT),
+  database: env.DB_NAME,
+  user: env.DB_USER,
+  password: env.DB_PASSWORD,
+});
+
+export async function query<T extends QueryResultRow = QueryResultRow>(
+  text: string,
+  params: unknown,
+): Promise<QueryResult<T>> {
+  const result = await pool.query<T>(text, params as any[]);
+
+  return result;
+}
+
+export async function assertDatabaseConnection() {
+  try {
+    await pool.query('SELECT 1;');
+    
+    logger.info('Connected to PostgreSQL')
+  } catch (err) {
+    throw err;
+  }
+}
