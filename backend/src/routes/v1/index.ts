2cdaff16e9024613a5cb71895d6d9751a19d12d8
Tunde James <59791126+tunde-james@users.noreply.github.com>
2026-02-01 21:18:18 +0100

    Add the chat table
    Add methods for creating chats, listing direct messages, and
    listing chat users
    
    Add chat routes

--- backend/src/routes/v1/index.ts
+++ backend/src/routes/v1/index.ts
@@ -3,9 +3,11 @@ import { Router } from 'express';
 import { userRouter } from './user.routes';
 import { threadsRouter } from './threads.routes';
 import { notificationRouter } from './notifications.routes';
+import { chatRouter } from './chat.routes';
 
 export const apiRouter = Router();
 
 apiRouter.use('/users/me', userRouter);
 apiRouter.use('/threads', threadsRouter);
 apiRouter.use('/notifications', notificationRouter);
+apiRouter.use('/chat', chatRouter);
